634a02079a4b2dd1100e3b695516b22be90799f2
ywbird <ywbird@naver.com>
2023-07-22 04:34:28 +0900

    update page filter

--- popup/popup.js
+++ popup/popup.js
@@ -7,13 +7,13 @@ const iconAlt2 = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   const activeTab = tabs[0];
 
-  if (!activeTab.url.startsWith('https://www.youtube.com/')) {
+  if (!activeTab.url.startsWith('https://www.youtube.com/watch')) {
     const main = document.querySelector('main');
     main.innerHTML = '';
     const div = document.createElement('div');
     div.classList.add('notice');
     div.innerHTML =
-      '<span>This extension is only for Youtube.</span><svg class="notice-svg" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>YouTube</title><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>';
+      '<span>This extension is only for Youtube watch page.</span><svg class="notice-svg" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>YouTube</title><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>';
     main.appendChild(div);
   }
 
